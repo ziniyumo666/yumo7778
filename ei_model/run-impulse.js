568e01395a2b685ff918b276fe23ac47f6b31e19
ziniyumo666 <161329238+ziniyumo666@users.noreply.github.com>
2025-05-17 19:57:24 +0800

    Update run-impulse.js

--- ei_model/run-impulse.js
+++ ei_model/run-impulse.js
@@ -2,22 +2,22 @@
 const Module = require('./edge-impulse-standalone');
 
 module.exports = async function () {
-  await Module.ready;
-  Module.init();
-
-  return {
-    classify: (float32RGBInput) => {
-      const typed = new Float32Array(float32RGBInput);
-      const nBytes = typed.length * typed.BYTES_PER_ELEMENT;
-      const ptr = Module._malloc(nBytes);
-      const heap = new Uint8Array(Module.HEAPU8.buffer, ptr, nBytes);
-      heap.set(new Uint8Array(typed.buffer));
-      const result = Module.run_classifier(ptr, typed.length, false);
-      Module._free(ptr);
-      return result;
-    }
-  };
+  return new Promise((resolve) => {
+    Module.onRuntimeInitialized = () => {
+      resolve({
+        classify: (input) => {
+          const typed = new Float32Array(input);
+          const nBytes = typed.length * typed.BYTES_PER_ELEMENT;
+          const ptr = Module._malloc(nBytes);
+          const heap = new Uint8Array(Module.HEAPU8.buffer, ptr, nBytes);
+          heap.set(new Uint8Array(typed.buffer));
+          const result = Module.run_classifier(ptr, typed.length, false);
+          Module._free(ptr);
+          return result;
+        }
+      });
+    };
+  });
 };
-
 
 
